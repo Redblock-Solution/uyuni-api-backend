8175b34b0909a9e53fef999904e737d0978ccef4
Kaique Yamamoto <kaique@elasticcode.com.br>
2024-05-12 11:50:00 -0300

    fixed: create add core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('login/', views.obtain_auth_token),
     path('users/', include('users.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
 
 
